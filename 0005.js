329b2640816b6abbd4964cfb75f2f4b8bfe3c173
Keith Yong <keithy@wifi-roaming-128-4-76-195.host.udel.edu>
2014-10-23 14:53:23 -0400

    Added solution to problem 5

---
+++ 0005.js
@@ -0,0 +1,28 @@
+/* Prime factorization
+ * 20 = 2^2 * 5
+ * 19 = 19
+ * 18 = 2 * 3^2
+ * 17 = 17
+ * 16 = 2^4
+ * 15 = 5 * 3
+ * 14 = 7 * 2
+ * 13 = 13
+ * 12 = 3 * 2 * 2
+ * 11 = 11
+ * 10 = 5 * 2
+ * 9 = 3 * 3
+ * 8 = 2^3
+ * 7 = 7
+ * 6 = 3 * 2
+ * 5 = 5
+ * 4 = 2 * 2
+ * 3 = 3
+ * 2 = 2
+ * 1 = 1
+ * 
+ * The lowest number must be able to provide the prime factors of the numbers
+ * 1 - 20.
+ * 
+ * So, answer is
+ * 2^4 * 3^2 * 5 * 7 * 11 * 13 * 17 * 19
+ */
