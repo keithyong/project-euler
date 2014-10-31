47a36a9829096767da903956d0e9e5afd958511c
Keith Yong <terda12@gmail.com>
2014-10-31 00:14:27 -0400

    Added parsing function in order to take in many lines of integers and put them into a 2D array.

--- problem18/js/script.js
+++ problem18/js/script.js
@@ -1,16 +1,31 @@
 var textInput;
+var line;
 var triangle;
 var done = false;
 
 function main(){
     var start = new Date().getTime();
     textInput = document.getElementById("input").value;
-    triangle = textInput.split("\n");
+
+    triangle = parse(textInput);
     document.getElementById("output").innerHTML = triangle;
     elapsed = new Date().getTime() - start;
+
     document.getElementById("runningTime").innerHTML = "Running time <br />" + elapsed + " millisecond(s)";
 } 
 
-QUnit.test("hello test", function( assert ){
-    assert.ok( 1 == "1", "Passed!" );
+function parse(input){
+    var inputArray = input.split("\n"); 
+    var temp2D = [];
+
+    for (var i = 0; i < inputArray.length; i++){
+        temp2D.push(inputArray[i].split(" ").map(Number));
+    }
+
+    return temp2D;
+}
+
+QUnit.test('parse()', function( assert ){
+    var testArray = [[1, 2, 3],[4, 5, 6]];
+    assert.deepEqual(parse("1 2 3\n4 5 6"), testArray, "Parse works!");
 });
