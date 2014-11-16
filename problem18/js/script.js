587cd70b17d26b92f693ecdb53aaa0e8dfce9ffa
Keith Yong <terda12@gmail.com>
2014-11-16 07:10:11 -0500

    A lot of bug fixes, but dijkstra does not work correctly still.

--- problem18/js/script.js
+++ problem18/js/script.js
@@ -3,7 +3,7 @@ var line;
 var triangle;
 var done = false;
 var MAXINT = 9007199254740992;
-
+var MININT = -9007199254740992;
 function main(){
     var start = new Date().getTime();
 
@@ -15,15 +15,38 @@ function main(){
 
     document.getElementById("runningTime").innerHTML = "Running time <br />" + elapsed + " millisecond(s)";
     var triangleAdjMat = makeAdjMatTriangle(triangle);
+    var vertexWeights = makeWeightArray(triangle);
     write2DArray(triangleAdjMat);
-    maxDijkstra(triangleAdjMat, 0);
+    var dist = maxDijkstra(triangleAdjMat, vertexWeights, 0);
+    var maxIndex = findIndexWithMax(dist);
+    document.getElementById("output").innerHTML = "Found max path! It is " + dist[maxIndex];
 } 
 
 function write2DArray(arr){
     var html = '';
     for (var i = 0; i < arr.length; i++)
          html += arr[i] + "<br />";
-    document.getElementById("output").innerHTML = html;
+    document.getElementById("output").innerHTML += html;
+}
+
+function writeArray(arr){
+    var html = '';
+    for (var i = 0; i < arr.length; i++)
+        html += arr[i] + ",";
+    html += "<br />";
+    document.getElementById("output").innerHTML += html;
+}
+
+function findIndexWithMax(arr){
+    var max = MININT;
+    var index = -1;
+    for (var i = 0; i < arr.length; i++){
+        if (max < arr[i]){
+            max = arr[i];
+            index = i;
+        }
+    }
+    return index;
 }
 
 function parse(input){
@@ -92,13 +115,28 @@ function makeAdjMatTriangle(tri2DArray) {
     return adjmat;
 }
 
+// Given a 2D array of the triangle, returns a vertex weight array
+function makeWeightArray(tri2DArr){
+    var vertexWeights = new Array(getNumNodes(tri2DArr));;
+    var count = 0;
+    for(var i = 0; i < tri2DArr.length; i++){
+        for (var j = 0; j < tri2DArr[i].length; j++){
+            vertexWeights[count] = tri2DArr[i][j];
+            count++;
+        }
+    }
+    return vertexWeights;
+}
+
 // From the set of vertices not in the tree and not infinite, find the vertex with the
 // biggest distance
 function maxDistance(dist, intree)
 {
-    var max = -1;
+    var max = 0;
     var maxV = -1;
 
+    // For all vertices, if v is not in the dijkstra MST
+    // and max distance sofar is less than or equal to dist[v]
     for (var v = 0; v < dist.length; v++){
         if (intree[v] === false && max <= dist[v]){
             max = dist[v];
@@ -108,44 +146,42 @@ function maxDistance(dist, intree)
     return maxV;
 }
 // Maximal dijkstra may not work if there is a cycle, but since we don't have cycles then that's fine
-// Adapted from Skiena 208
-function maxDijkstra(adjmat, start) {
-    var i;          /* Counter */
+function maxDijkstra(adjmat, weight, start) {
     var nvertices = adjmat.length;
     var intree = new Array(nvertices);
-    var distance = new Array(nvertices);    // distance[i] = maximum distance from start -> i
-    var nbrs;
+    var distance = new Array(nvertices);
     var v;
-    var w;
-    var weight;
-    var dist;
 
     for(var i = 0; i <= nvertices; i++){
         intree[i] = false;
-        distance[i] = -MAXINT;
+        distance[i] = MININT;
     }
 
-    distance[start] = 0;
-    intree[v] = 1;
+    distance[start] = 3;
     var u;  // Candidate vertex
-    for(var c = 0; c < nvertices - 1; c++){
+    for(var c = 0; c <= nvertices; c++){
         u = maxDistance(distance, intree);
+        console.log("u = " + u);
         intree[u] = true;
 
+        writeArray(distance);
         for (var v = 0; v < nvertices; v++){
             //If
             //1. vertex v is not in the SP tree yet
             //2. there is an edge between u and v
-            //3. distance of u is not -MAXINT
+            //3. distance of u is not MININT
             //4. distance of v is less than the distance of u + weight of edge uv
             if (!intree[v] && 
                 adjmat[u][v] && 
-                distance[u] != -MAXINT && 
-                distance[v] < distance[u] + adjmat[u][v]){
-                distance[v] = distance[u] + adjmat[u][v];
+                distance[u] != MININT && 
+                distance[v] < distance[u] + weight[v]){
+                // Here we are using vertex weight instead of edge weight
+                distance[v] = distance[u] + weight[v];
             }
         }
     }
+
+    return distance;
 }
 
 // ----- TESTS ------
@@ -197,4 +233,16 @@ QUnit.test('getLeftChildIndex()', function( assert ){
     assert.equal(getLeftChildIndex(0, 1), 1);
     assert.equal(getLeftChildIndex(10, 5), 15);
     assert.equal(getLeftChildIndex(21, 7), 28);
+});
+
+QUnit.test('makeWeightArray()', function( assert ){
+    var correctWeightArray = [59, 73, 41, 52, 40, 9];
+    assert.deepEqual(makeWeightArray(testArray), correctWeightArray);
+});
+
+QUnit.test('findIndexWithMax()', function( assert ){
+    var test = [2, 3, 4, 5, 3, 5, 2, 9001, 2, 4];
+    var test2 = [9000, 9000, 9000, 9000, 9001, 9000, 9000];
+    assert.equal(findIndexWithMax(test), 7);
+    assert.equal(findIndexWithMax(test2), 4);
 });
